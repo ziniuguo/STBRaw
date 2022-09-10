5a56d7af439126c67f10d9e38bd1a1d4d0fa87d5
wangyanbao666 <yanbao_wang@mymail.sutd.edu.sg>
2022-09-10 15:58:12 +0800

    update for homepage layout

--- app/src/main/java/com/example/stbraw/MainActivity.java
+++ app/src/main/java/com/example/stbraw/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,16 +30,27 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements AsyncResponse {
     DownloadFromInternet dd = new DownloadFromInternet();
-    TextView predictionView;
+    TextView predictionView1;
+    TextView predictionView2;
+    TextView predictionView3;
     Button upload_img_btn;
+    ImageView uploaded_img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        predictionView = findViewById(R.id.prediction);
+        predictionView1 = findViewById(R.id.prediction1);
+        predictionView2 = findViewById(R.id.prediction2);
+        predictionView3 = findViewById(R.id.prediction3);
+
+//        predictionView1.setVisibility(View.INVISIBLE);
+//        predictionView2.setVisibility(View.INVISIBLE);
+//        predictionView3.setVisibility(View.INVISIBLE);
+
         upload_img_btn = findViewById(R.id.upload_img_btn);
+        uploaded_img = findViewById(R.id.uploaded_img);
 
         dd.delegate = this;
         dd.execute();
@@ -68,11 +80,13 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
                             && data.getData() != null) {
                         Uri selectedImageUri = data.getData();
                         Bitmap selectedImageBitmap;
+
                         try {
                             selectedImageBitmap
                                     = MediaStore.Images.Media.getBitmap(
                                     this.getContentResolver(),
                                     selectedImageUri);
+                            uploaded_img.setImageBitmap(selectedImageBitmap);
                             processFinish(selectedImageBitmap);
                             Toast.makeText(MainActivity.this, "Details Saved Successfully.", Toast.LENGTH_SHORT).show();
                         } catch (IOException e) {
@@ -96,13 +110,23 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
             BagModel.Outputs outputs = model.process(image);
             List<Category> probability = outputs.getProbabilityAsCategoryList();
 
+            int count = 0;
             for (Category prob : probability) {
+                if (count > 2){
+                    break;
+                }
+                count+=1;
                 result.add(new Recognition(prob.getLabel(), prob.getScore()));
             }
             // Releases model resources if no longer used.
             model.close();
 
-            predictionView.setText(result.toString());
+            predictionView1.setText(result.get(0).toString());
+            predictionView2.setText(result.get(1).toString());
+            predictionView3.setText(result.get(2).toString());
+            predictionView1.setVisibility(View.VISIBLE);
+            predictionView2.setVisibility(View.VISIBLE);
+            predictionView3.setVisibility(View.VISIBLE);
 
         } catch (IOException e) {
             e.printStackTrace();
